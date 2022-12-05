07043c41f3fc29d8c8763308f81eed8e636e271a
Ciara Molloy <20089106@mail.wit.ie>
2022-12-05 22:21:17 +0000

    Initial express App

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,7 +1,5 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import moviesRouter from './api/movies';
-import genresRouter from './api/genres';
 
 dotenv.config();
 
@@ -9,11 +7,7 @@ const app = express();
 
 const port = process.env.PORT;
 
-app.use(express.json());
-
-app.use('/api/movies', moviesRouter);
-
-app.use('/api/genres', genresRouter);
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
